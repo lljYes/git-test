4587f1ac8ce113b153dac3d39f9b1202b3b00940
llj <lljDdos@163.com>
2022-11-03 15:32:48 +0800

    second commit

--- src/main/java/com/atguigu/test/git/GitTest.java
+++ src/main/java/com/atguigu/test/git/GitTest.java
@@ -3,6 +3,7 @@ package com.atguigu.test.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git!!");
+        System.out.println("hello git second!");
     }
 
 }
