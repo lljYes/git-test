c33bc74a2c7e9c40633ab51e72986c0f7db547c1
llj <lljDdos@163.com>
2022-11-03 15:48:10 +0800

    hot-fix commit1

--- src/main/java/com/atguigu/test/git/GitTest.java
+++ src/main/java/com/atguigu/test/git/GitTest.java
@@ -3,8 +3,9 @@ package com.atguigu.test.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git!!");
-        System.out.println("hello git 22!");
-        System.out.println("hello git 33!");
+        System.out.println("hello git 2!");
+        System.out.println("hello git 3!");
+        System.out.println("hello git 4!");
     }
 
 }
