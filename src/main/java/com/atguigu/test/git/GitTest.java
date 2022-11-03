2342e8b83ee506a30598f1403a6e939d1aa3ddae
llj <lljDdos@163.com>
2022-11-03 20:20:20 +0800

    push commit

--- src/main/java/com/atguigu/test/git/GitTest.java
+++ src/main/java/com/atguigu/test/git/GitTest.java
@@ -8,7 +8,7 @@ public class GitTest {
         System.out.println("hello git 4!");
         System.out.println("master test");
         System.out.println("hot-fix test");
-
+        System.out.println("push test");
     }
 
 }
